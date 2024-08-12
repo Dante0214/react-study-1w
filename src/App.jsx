fea9aefa599787d9315ca442d41f56e123a19a79
dante0214 <rn_chic@naver.com>
2024-08-12 15:22:12 +0900

    Feat : 결과 기능 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Box from "./components/Box";
 import "./App.css";
 import { useState } from "react";
+import rockImg from "./assets/images/rock.png";
+import scissorsImg from "./assets/images/scissors.png";
+import paperImg from "./assets/images/paper.png";
 
 // 박스 2개{타이틀,사진,결과}
 // 가위 바위 보 버튼
@@ -8,11 +11,65 @@ import { useState } from "react";
 // 컴퓨터는 랜덤하게 선택
 // 결과를 가지고 누가 이겼는지 승패을 따짐
 // 결과에 따라 테두리 색이 바뀜
+const choice = {
+  rock: {
+    name: "Rock",
+    img: rockImg,
+  },
+  scissors: {
+    name: "Scissors",
+    img: scissorsImg,
+  },
+  paper: {
+    name: "Paper",
+    img: paperImg,
+  },
+};
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const play = (userChoice) => {
+    const computerChoice = randomChoice();
+
+    setUserSelect(choice[userChoice]);
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+  const randomChoice = () => {
+    const itemArray = Object.keys(choice);
+    const randomItem = Math.floor(Math.random() * itemArray.length);
+    const final = itemArray[randomItem];
+    return choice[final];
+  };
+
+  //무승부 상황을 제외 후,
+  //가위 -> 보, 바위 -> 가위, 보-> 가위 이외에 모든 상황은 패배임
+
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "Tie";
+    } else if (
+      (user.name === "Rock" && computer.name === "Scissors") ||
+      (user.name === "Scissors" && computer.name === "Paper") ||
+      (user.name === "Paper" && computer.name === "Rock")
+    ) {
+      return "Win";
+    } else {
+      return "Lose";
+    }
+  };
   return (
     <div>
-      <Box name="You" />
-      <Box name="Coumputer" />
+      <div className="main">
+        <Box name="You" item={userSelect} result={result} />
+        <Box name="Computer" item={computerSelect} result={result} />
+      </div>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 const Box = (props) => {
+  let result;
+  if (
+    props.name === "Computer" &&
+    props.result !== "Tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "Win" ? "Lose" : "Win";
+  } else {
+    result = props.result;
+  }
   return (
     <div className="box">
       <h1>{props.name}</h1>
-      <img src="" />
-      <h2>{props.result}</h2>
+      <img
+        className="item-img"
+        src={props.item && props.item.img}
+        alt={props.item && props.item.name}
+      />
+      <h2>{result}</h2>
     </div>
   );
 };
